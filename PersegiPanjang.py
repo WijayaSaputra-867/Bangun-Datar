fd8639caca4db817fe52b1be71cee271b0eb7d1c
wijayasaputra-867 <wijayasaputra679@gmail.com>
2021-11-09 11:32:28 +0700

    Menambahkan kondisi di fungsi ulang luas

---
+++ PersegiPanjang.py
@@ -0,0 +1,195 @@
+def UlangLuas(panjang, lebar) :
+    onoff = True
+    while onoff :
+        ulang = input("""\nApakah anda mau menghitung ulang lagi? 
+    1. ya | 2. tidak
+    3. Menghitung keliling dari panjang dan lebar yang sama? : sama
+    4. Menghitung keliling dari panjang dan lebar yang baru? : baru 
+    """)
+        if ulang.casefold() == "ya" or ulang == "1":
+            HitungLuasPersegiPanjang()
+            onoff = True
+        elif ulang.casefold() == "tidak" or ulang == "2":
+            onoff = False
+            print("\nTerima kasih :-)\n")
+            exit()
+        elif ulang.casefold() == "sama" or ulang == "3":
+            HitungKelilingdariLuas(panjang, lebar)
+        elif ulang.casefold() == "baru" or ulang == "4":
+            HitungKelilingPersegiPanjang()
+        elif ulang.casfold() == "panjang" or ulang == "5":
+            HitungPanjangDariLuas()
+        elif ulang.casefold() == "lebar" or ulang == "6":
+            HitungLebardariLuas()
+        else :
+            print("\nHarus memilih salah satu yang ada diatas!")
+            onoff = True
+
+        
+def UlangKeliling(panjang, lebar) : 
+    onoff = True
+    while onoff :
+        ulang = input("""\nApakah anda mau menghitung ulang lagi? 
+    1. ya | 2. tidak
+    3. Menghitung luas dari panjang dan lebar yang sama? : sama
+    4. Menghitung luas dari panjang dan lebar yang baru? : baru 
+    """)
+        if ulang.casefold() == "ya" or ulang == "1":
+            HitungKelilingPersegiPanjang()
+            onoff = True
+        elif ulang.casefold() == "tidak" or ulang == "2" :
+            onoff = False
+            print("\nTerima kasih :-)\n")
+            exit()
+        elif ulang.casefold() == "sama" or ulang == "3":
+            HitungLuasdariKeliling(panjang, lebar)
+        elif ulang.casefold() == "baru" or ulang == "4":
+            HitungLuasPersegiPanjang()
+        else :
+            print("\nHarus memilih salah satu yang ada diatas!")
+            onoff = True
+
+def HitungLuasPersegiPanjang() :
+        inputPanjang = input("\nMasukan Panjang : ")
+        inputLebar = input("Masukan Lebar : ")
+        if inputPanjang.isdecimal() & inputLebar.isdecimal() :
+            panjang = int(inputPanjang)
+            lebar = int(inputLebar)
+
+            if panjang > lebar :
+                hasil = panjang * lebar
+                print("Luas = Panjang * Lebar\nLuas = ", panjang, " * ", lebar)
+                print("Luasnya adalah = ", hasil)
+                UlangLuas(panjang, lebar)
+
+            else :
+                print("\nPanjang harus Lebih besar dari lebar!")
+                HitungLuasPersegiPanjang()
+
+        else :
+            print("\nKeduanya harus menggunakan bilangan bulat!")
+            HitungLuasPersegiPanjang()
+
+def HitungKelilingPersegiPanjang() :
+        inputPanjang = input("\nMasukan Panjang : ")
+        inputLebar = input("Masukan Lebar : ")
+        if inputPanjang.isdecimal() & inputLebar.isdecimal() :
+            panjang = int(inputPanjang)
+            lebar = int(inputLebar)
+
+            if panjang > lebar :
+                hasil = (panjang + lebar) * 2
+                print("Keliling = (Panjang + Lebar) * 2\nKeliling = (", panjang, " + ", lebar, " * 2")
+                print("Kelilingnya adalah = ", hasil)
+                UlangKeliling(panjang, lebar)
+
+            else :
+                print("\nPanjang harus Lebih besar dari lebar!")
+                HitungKelilingPersegiPanjang()
+
+        else :
+            print("\nKeduanya harus menggunakan bilangan bulat!")
+            HitungKelilingPersegiPanjang()
+
+def HitungKelilingdariLuas(panjang, lebar) :
+    hasil = (panjang + lebar) * 2
+    print("Keliling = (Panjang + Lebar) * 2\nKeliling = (", panjang, " + ", lebar, " ) * 2")
+    print("Kelilingnya adalah = ", hasil)
+    UlangKeliling(panjang, lebar)
+
+def HitungLuasdariKeliling(panjang, lebar) :
+    hasil = panjang * lebar
+    print("Luas = Panjang * Lebar\nLuas = ", panjang, " * ", lebar)
+    print("Luasnya adalah = ", hasil)
+    UlangLuas(panjang, lebar)
+
+def HitungPanjangDariLuas() :
+    inputLuas = input("\nMasukan Luas : ")
+    inputLebar = input("Masukan Lebar : ")
+    if inputLuas.isdecimal() & inputLebar.isdecimal() :
+        luas = int(inputLuas)
+        lebar = int(inputLebar)
+        if luas > lebar :
+            panjang = luas / lebar
+            print("Panjang = luas / lebar\nPanjang = ", luas, " / ", lebar)
+            print("Panjangnya adalah = ", panjang)
+            UlangPanjangdariLuas()
+        else :
+            print("\nLuas harus lebih besar dari lebar!")
+            HitungPanjangDariLuas()
+    else :
+        print("\nKeduanya harus menggunakan bilangan bulat!")
+        HitungPanjangDariLuas()
+
+def HitungLebardariLuas() :
+    inputLuas = input("\nMasukan Luas : ")
+    inputPanjang = input("Masukan Panjang : ")
+    if inputLuas.isdecimal() & inputPanjang.isdecimal() :
+        luas = int(inputLuas)
+        panjang = int(inputPanjang)
+        if luas > panjang :
+            lebar = luas / panjang
+            print("Lebar = luas / lebar\nLebar = ", luas, " / ", panjang)
+            print("Lebarnya adalah = ", lebar)
+            UlangLebardariLuas()
+        else :
+            print("\nLuas harus lebih besar dari panjang!")
+            HitungLebardariLuas()
+    else :
+      print("\nKeduanya harus menggunakan bilangan bulat!")
+      HitungLebardariLuas()
+
+
+def PersegiPanjang() :
+    onoff = True
+    while onoff :
+        Pilih = input("Menghitung luas atau keliling persegi panjang :\n1. Luas | 2. Keliling ")
+        if Pilih.casefold() == "luas" or Pilih == "1" :
+            Luas()
+            onoff = False
+        elif Pilih.casefold() == "keliling" or Pilih == "2" :
+            Keliling()
+            onoff = False
+        else :
+            print("\nJika menggunakan bilangan harus memilih 1 atau 2!\nJika menggunakan kalimat harus memilih luas atau keliling!")
+            onoff = True
+
+def Luas() : 
+    Input_user = input(""""
+    1. Hitung luas : luas
+    2. Hitung lebar : lebar
+    3. Hitung panjang : panjang
+    """)
+    if Input_user == "1" or Input_user.casefold() == "luas" :
+        HitungLuasPersegiPanjang()
+    elif Input_user == "2" or Input_user.casefold() == "lebar" :
+        HitungLuasPersegiPanjang()
+    elif Input_user == "3" or Input_user.casefold() == "panjang" :
+        HitungPanjangDariLuas()
+    else :
+        print("Harap memilih salah satu yang ada diatas!")
+        Luas()
+        
+def Keliling() :
+    Input_user = input("""
+    1. Hitung Keliling : keliling
+    2. Hitung lebar : lebar
+    3. Hitung panjang : panjang
+    """)
+    if Input_user == "1" or Input_user.casefold() == "keliling" :
+        HitungKelilingPersegiPanjang()
+    elif Input_user == "2" or Input_user.casefold() == "lebar" :
+        HitungLebardariKeliling()
+    elif Input_user == "3" or Input_user.casefold() == "panjang" :
+        HitungPanjangdariLebar()
+    else :
+        print("Harap memilih salah satu yang ada diatas!")
+        keliling()
+
+def UlangLebardariLuas() :
+    print("test")
+
+def UlangPanjangdariLuas() :
+    print("test")
+
+PersegiPanjang()
